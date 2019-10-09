d3c0af73cc1c28bebb5db7a6a5c46c65f86f6f41
subhahu123 <jainsubhahu.gmail.com>
2019-10-09 23:22:58 +0530

    adding requesting

--- script.js
+++ script.js
@@ -2,17 +2,22 @@
 const input = document.getElementById('fileupload');
 const image = document.querySelector('#image');
 
+var formData = new FormData();
+
+
+
 // This will upload the file after having read it
 const upload = (file) => {
 
-    fetch('192.168.0.118:8080/set', { // Your POST endpoint
+    formData.append("file", file);
+    formData.append('mode', '1');
+
+    console.log(input.files[0]);
+
+
+    fetch('http://192.168.0.118:8080/set', { // Your POST endpoint
         method: 'POST',
-        headers: {
-            // Content-Type may need to be completely **omitted**
-            // or you may need something
-            "Content-Type": "You will perhaps need to define a content-type here"
-        },
-        body: file // This is your file object
+        body: formData // This is your file object
     }).then(
         response => response.json() // if the response is a JSON object
     ).then(
@@ -34,7 +39,7 @@ const onSelectFile = () => {
 
     reader.readAsDataURL(input.files[0]);
 
-    return upload(input.files[0]);
+    upload(input.files[0]);
 }
 
 
